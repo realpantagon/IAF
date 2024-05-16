0761588c07fe89f75bd3da65c742015088e2aa95
realpantagon <pichayoothunchainao@gmail.com>
2024-05-16 18:47:24 +0700

    remove

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,12 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStateProvider } from "./GlobalStateIn";
-import Doorscan from "./Doorscan";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <GlobalStateProvider>
       <App />
-      {/* <Doorscan/> */}
     </GlobalStateProvider>
   </BrowserRouter>
 );
