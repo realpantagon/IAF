2bb7018100ee13744ffbece449ecb0e2d0542f2c
realpantagon <pichayoothunchainao@gmail.com>
2024-05-24 14:00:16 +0700

    wait for test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Dashboard from "./Dashboard";
 import Dashboard2 from "./Dashboard2";
 import Scanin2 from "./Scanin2";
 import Scanout2 from "./Scanout2";
-import Scaninmain2 from "./Scaninmain2";
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
         <Route path="Scanoutmain" element={<Scanout />} />
         <Route path="Scaninpitch" element={<Scanin2 />} />
         <Route path="Scanoutpitch" element={<Scanout2 />} />
-        <Route path="Scaninmain2" element={<Scaninmain2 />} />
       </Routes>
     </div>
   );
